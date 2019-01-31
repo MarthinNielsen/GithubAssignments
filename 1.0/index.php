625121d490cfccc494a773885ec2e0a656092843
MarthinNielsen <MarthinNielsen@hotmail.com>
2019-01-31 20:22:59 +0100

    hm

---
+++ 1.0/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Testsite</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<header>
+		
+	</header>
+
+	<main>
+		
+	</main>
+
+	<footer>
+		
+	</footer>
+</body>
+</html>
